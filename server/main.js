2ec7d25e463f33fbc9d15e3419cb837aa846f8f8
Chris Ariagno <TheGuyWhoCodes@users.noreply.github.com>
2019-02-09 10:29:28 -0600

    WE ARE IN

---
+++ server/main.js
@@ -0,0 +1,20 @@
+var admin = require("firebase-admin");
+const fs = require('fs');
+path = require('path');
+var serviceAccount = require("./serviceAccountKey.json");
+filePath = path.join(__dirname, 'test.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://hawk-calculator.firebaseio.com"
+});
+
+
+fs.readFile(filePath, "utf8",function(err,data){
+    if(err){
+        throw err;
+    }
+    admin.database().ref("/data/").update({
+        json: data
+    })
+});
